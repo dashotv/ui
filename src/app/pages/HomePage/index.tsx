dbb8ec23c5b3910edb70de354386f413b2b32939
Shawn Catanzarite <me@shawncatz.com>
2022-09-28 00:07:17 -0700

    switch to axios

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import axios from 'axios';
 import {Helmet} from 'react-helmet-async';
 import {Masthead} from './Masthead';
 import {Features} from './Features';
@@ -9,35 +10,27 @@ export function HomePage() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
   useEffect(() => {
-    fetch(`/api/tower/upcoming/`)
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error(
-                `This is an HTTP error: The status is ${response.status}`
-            );
-          }
-          return response.json();
-        })
-        .then((actualData) => {
-          console.log(actualData);
-          setData(actualData);
-          setError(null);
-        })
-        .catch((err) => {
-          setError(err.message);
-          setData([]);
-        })
-        .finally(() => {
-          setLoading(false);
-        });
+    const getData = async () => {
+      try {
+        const response = await axios.get('/api/tower/upcoming/');
+        setData(response.data);
+        setError(null);
+      } catch (err) {
+        // @ts-ignore
+        setError(err.message);
+        setData([]);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getData();
   }, []);
 
   return (
       <>
         <Helmet>
-          <title>Home Page</title>
+          <title>Home</title>
           <meta
               name="description"
               content="A React Boilerplate application homepage"
@@ -45,14 +38,14 @@ export function HomePage() {
         </Helmet>
         <PageWrapper>
           <div>
-            {loading && <div>A moment please...</div>}
+            {loading && <div>loading...</div>}
             {error && (
                 <div>{`There is a problem fetching the post data - ${error}`}</div>
             )}
             <ul>
-              {data && data.map(({ id, title, search, season_number, episode_number }) => (
+              {data && data.map(({ id, title, search, season_number, episode_number, release_date }) => (
                       <li key={id}>
-                        <h3>{search} {season_number}x{episode_number} {title}</h3>
+                        <h3>{search} {season_number}x{episode_number} {title} {release_date}</h3>
                       </li>
                   ))}
             </ul>
