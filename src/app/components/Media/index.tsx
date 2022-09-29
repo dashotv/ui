cea383ca9dd57fe8e26f09f473603c6c4b65eb6b
Shawn Catanzarite <me@shawncatz.com>
2022-09-29 14:53:09 -0700

    add blank image to fill empty spot, add links to home page

--- src/app/components/Media/index.tsx
+++ src/app/components/Media/index.tsx
@@ -1,30 +1,36 @@
-import * as React from "react";
-import ImageList from "@mui/material/ImageList";
-import ImageListItem from "@mui/material/ImageListItem";
-import ImageListItemBar from "@mui/material/ImageListItemBar";
-
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
 
 export default function Media(props) {
+  function setDefaultSrc(ev) {
+    ev.target.src = '/blank.png';
+  }
   return (
-      <ImageList cols={5}>
-        {props.loading && <div>loading...</div>}
-        {props.error && (
-            <div>{`There is a problem fetching the post data - ${props.error}`}</div>
-        )}
-        {props.data.map(({ id, title, search, display, cover }) => (
-            <ImageListItem key={id}>
-              <img
-                  src={`${cover}?w=135&h=200`}
-                  alt="cover"
-                  loading="lazy"
-              />
-              <ImageListItemBar
-                  title={title}
-                  subtitle={display}
-                  sx={{ display: { xs: 'none', md: 'flex' } }}
-              />
-            </ImageListItem>
-        ))}
-      </ImageList>
-  )
+    <ImageList cols={5}>
+      {props.loading && <div>loading...</div>}
+      {props.error && (
+        <div>{`There is a problem fetching the post data - ${props.error}`}</div>
+      )}
+      {props.data.map(({ id, title, series_id, display, cover }) => (
+        <Link key={id} to={`/media/series/${series_id}`}>
+          <ImageListItem>
+            <img
+              src={`${cover}?w=135&h=200`}
+              alt="cover"
+              loading="lazy"
+              onError={setDefaultSrc}
+            />
+            <ImageListItemBar
+              title={title}
+              subtitle={display}
+              sx={{ display: { xs: 'none', md: 'flex' } }}
+            />
+          </ImageListItem>
+        </Link>
+      ))}
+    </ImageList>
+  );
 }
--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import axios from 'axios';
-import {Helmet} from 'react-helmet-async';
+import { Helmet } from 'react-helmet-async';
 import Container from '@mui/material/Container';
 import Media from '../../components/Media';
-import {LoadingIndicator, LoadingWrapper} from "../../components/LoadingIndicator";
+import { Link } from 'react-router-dom';
+import {
+  LoadingIndicator,
+  LoadingWrapper,
+} from '../../components/LoadingIndicator';
 
-import {useEffect, useState} from "react";
+import { useEffect, useState } from 'react';
 
 export function HomePage() {
   const [data, setData] = useState([]);
@@ -16,12 +20,11 @@ export function HomePage() {
     const getData = async () => {
       try {
         const response = await axios.get('/api/tower/upcoming/');
+        console.log(response.data);
         setData(response.data);
-        setError(null);
       } catch (err) {
         // @ts-ignore
         setError(err.message);
-        setData([]);
       } finally {
         setLoading(false);
       }
@@ -30,25 +33,25 @@ export function HomePage() {
   }, []);
 
   return (
-      <>
-        <Helmet>
-          <title>Home</title>
-          <meta
-              name="description"
-              content="A React Boilerplate application homepage"
-          />
-        </Helmet>
-        <Container>
-          {loading && (
-              <LoadingWrapper>
-                <LoadingIndicator />
-              </LoadingWrapper>
-          )}
-          {error && (
-              <div>{`There is a problem fetching the post data - ${error}`}</div>
-          )}
-          {data && (<Media data={data} />)}
-        </Container>
-      </>
+    <>
+      <Helmet>
+        <title>Home</title>
+        <meta
+          name="description"
+          content="A React Boilerplate application homepage"
+        />
+      </Helmet>
+      <Container>
+        {loading && (
+          <LoadingWrapper>
+            <LoadingIndicator />
+          </LoadingWrapper>
+        )}
+        {error && (
+          <div>{`There is a problem fetching the post data - ${error}`}</div>
+        )}
+        {data && <Media data={data} />}
+      </Container>
+    </>
   );
 }
