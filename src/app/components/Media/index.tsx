6e6509c77e15268b70971c92a8017973e91fcb83
Shawn Catanzarite <me@shawncatz.com>
2022-10-03 17:17:45 -0700

    move medium into it's own component

--- src/app/components/Media/index.tsx
+++ src/app/components/Media/index.tsx
@@ -5,97 +5,23 @@ import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
 import styled from 'styled-components/macro';
-import './styles.css';
+import { MediumSmall } from '../Medium';
 
 export default function Media(props) {
   return (
     <div>
       {props.data.map(
         ({ id, title, series_id, display, cover, release_date }) => (
-          <Medium key={id} id={series_id || id} title={title} display={display}>
-            <Background image={cover} />
-            <Header text={release_date} />
-            <Footer primary={title} secondary={display} />
-          </Medium>
+          <MediumSmall
+            key={id}
+            id={series_id || id}
+            background={cover}
+            primary={title}
+            secondary={display}
+            release={release_date}
+          />
         ),
       )}
     </div>
   );
 }
-
-function Footer(props) {
-  return (
-    <div className="footer">
-      <div className="primary">{props.primary}</div>
-      <div className="secondary">{props.secondary}</div>
-    </div>
-  );
-}
-
-function Header(props) {
-  const [date, setDate] = useState<string | null>(null);
-  function pad(num: number) {
-    return num.toString().padStart(2, '0');
-  }
-  useEffect(() => {
-    const inc = new Date(props.text);
-    const d = [
-      inc.getFullYear(),
-      pad(inc.getMonth() + 1),
-      pad(inc.getDate()),
-    ].join('-');
-    setDate(d);
-  }, []);
-  return (
-    <div className="header">
-      <div className="primary">{date}</div>
-    </div>
-  );
-}
-
-function Background(props) {
-  function setDefaultSrc(ev) {
-    ev.target.src = '/blank.png';
-  }
-  return (
-    <div className="background">
-      <img alt="cover" src={props.image} onError={setDefaultSrc} />
-    </div>
-  );
-}
-
-function Medium(props) {
-  return (
-    <div className="medium">
-      <Link to={`/media/series/${props.id}`}>{props.children}</Link>
-    </div>
-  );
-}
-// const Wrapper = styled.div`
-//   height: 295px;
-//   width: 200px;
-//   margin: 0.5rem;
-//   float: left;
-// `;
-
-/*
-        <ImageList cols={5}>
-          {props.data.map(({id, title, series_id, display, cover}) => (
-              <Link key={id} to={`/media/series/${series_id || id}`}>
-                <ImageListItem>
-                  <img
-                      src={`${cover}?w=135&h=200`}
-                      alt="cover"
-                      loading="lazy"
-                      onError={setDefaultSrc}
-                  />
-                  <ImageListItemBar
-                      title={title}
-                      subtitle={display}
-                      sx={{display: {xs: 'none', md: 'flex'}}}
-                  />
-                </ImageListItem>
-              </Link>
-          ))}
-        </ImageList>
-        */
---
+++ src/app/components/Medium/index.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import * as React from 'react';
+import './styles.css';
+
+export function MediumSmall(props) {
+  return (
+    <div className="medium">
+      <Link to={`/media/series/${props.id}`}>
+        <Background image={props.background} />
+        <Header text={props.release} />
+        <Footer primary={props.primary} secondary={props.secondary} />
+      </Link>
+    </div>
+  );
+}
+
+function Footer(props) {
+  return (
+    <div className="footer">
+      <div className="primary">{props.primary}</div>
+      <div className="secondary">{props.secondary}</div>
+    </div>
+  );
+}
+
+function Header(props) {
+  const [date, setDate] = useState<string | null>(null);
+  function pad(num: number) {
+    return num.toString().padStart(2, '0');
+  }
+
+  useEffect(() => {
+    const inc = new Date(props.text);
+    const d = [
+      inc.getFullYear(),
+      pad(inc.getMonth() + 1),
+      pad(inc.getDate()),
+    ].join('-');
+    setDate(d);
+  }, [date]);
+
+  return (
+    <div className="header">
+      <div className="primary">{date}</div>
+    </div>
+  );
+}
+
+function Background(props) {
+  function setDefaultSrc(ev) {
+    ev.target.src = '/blank.png';
+  }
+  return (
+    <div className="background">
+      <img alt="cover" src={props.image} onError={setDefaultSrc} />
+    </div>
+  );
+}
