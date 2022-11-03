f15ae33eb28dc1f98afd441d9c641042a041cadc
Shawn Catanzarite <me@shawncatz.com>
2022-11-03 15:33:31 -0700

    move loader to center and floated

--- src/app/components/Loading/Wrapper.tsx
+++ src/app/components/Loading/Wrapper.tsx
@@ -4,7 +4,13 @@ const Wrapper = styled.div`
   margin: 2em auto;
   width: 40px;
   height: 40px;
-  position: relative;
+  position: absolute;
+  margin-top: 40%;
+  top: 0px;
+  left: 0px;
+  bottom: 0px;
+  right 0px;
+  z-index: 1000;
 `;
 
 export default Wrapper;
