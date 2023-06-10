645b03697f1d216d485c8127faccd32912a75fda
Shawn Catanzarite <me@shawncatz.com>
2023-06-10 03:36:42 -0700

    move getDownloads to a callback so that we can update downloads when we receive a created message

--- src/app/pages/HomePage/UpcomingPage.tsx
+++ src/app/pages/HomePage/UpcomingPage.tsx
@@ -74,6 +74,7 @@ export default function UpcomingPage() {
         console.error(err);
         return;
       }
+
       const data = jc.decode(msg.data) as MediumEvent;
       if (data.episode.downloaded === true) {
         // if an episode is marked as downloaded (a download was created)
@@ -94,25 +95,33 @@ export default function UpcomingPage() {
         console.error(err);
         return;
       }
+
       const data = jc.decode(msg.data) as DownloadEvent;
+
+      if (data.event == 'created') {
+        // if a download is created, just get the downloads again
+        getDownloads();
+        return;
+      }
 
       if (data.event === 'destroyed' || data.download.status === 'done') {
         // if a download was destroyed or completed, remove from list
         setDownloads(prevState => prevState.filter(item => item.id !== data.id));
-      } else {
-        // otherwise, update the download that was changed
-        setDownloads(prevState => {
-          return prevState.map(item => {
-            if (item.id === data.id) {
-              item.status = data.download.status;
-              item.thash = data.download.thash;
-              item.url = data.download.url;
-              item.releaseId = data.download.releaseId;
-            }
-            return item;
-          });
-        });
+        return;
       }
+
+      // otherwise, update the download that was changed
+      setDownloads(prevState => {
+        return prevState.map(item => {
+          if (item.id === data.id) {
+            item.status = data.download.status;
+            item.thash = data.download.thash;
+            item.url = data.download.url;
+            item.releaseId = data.download.releaseId;
+          }
+          return item;
+        });
+      });
     },
     [jc],
   );
@@ -155,6 +164,19 @@ export default function UpcomingPage() {
     };
   }, [ws, handleNzbs, handleTorrents, handleNotices, handleDownloads, handleEpisodes]);
 
+  const getDownloads = useCallback(() => {
+    axios
+      .get('/api/tower/downloads/')
+      .then(response => {
+        // console.log(response.data);
+        setDownloads(response.data);
+      })
+      .catch(err => {
+        enqueueSnackbar('error getting data', { variant: 'error' });
+        console.error(err);
+      });
+  }, []);
+
   useEffect(() => {
     const getUpcoming = () => {
       setLoading(true);
@@ -164,19 +186,6 @@ export default function UpcomingPage() {
           // console.log(response.data);
           setUpcoming(response.data);
           setLoading(false);
-        })
-        .catch(err => {
-          enqueueSnackbar('error getting data', { variant: 'error' });
-          console.error(err);
-        });
-    };
-
-    const getDownloads = () => {
-      axios
-        .get('/api/tower/downloads/')
-        .then(response => {
-          // console.log(response.data);
-          setDownloads(response.data);
         })
         .catch(err => {
           enqueueSnackbar('error getting data', { variant: 'error' });
