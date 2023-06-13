66c6a6dea369a66a4de36d4db2b2c53ee1d80304
Shawn Catanzarite <me@shawncatz.com>
2023-06-13 14:30:05 -0700

    add roboto font

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,6 +5,10 @@
  * This component is the skeleton around the actual pages, and should only
  * contain code that should be seen on all pages. (e.g. navigation bar)
  */
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Route, Routes } from 'react-router-dom';
