90a23fcf8347d0dfa5c9d17d0c27b9a118416a56
Shawn Catanzarite <me@shawncatz.com>
2023-06-16 17:18:36 -0700

    DownloadWrapper should handle Download updates

--- src/app/components/Downloads/DownloadWrapper.tsx
+++ src/app/components/Downloads/DownloadWrapper.tsx
@@ -4,11 +4,12 @@ import * as React from 'react';
 import { useCallback, useEffect, useState } from 'react';
 
 import { Download } from '../../../types/download';
+import { useSubscription } from '../Nats/useSubscription';
 import { DownloadBanner } from './index';
 import { useReleases } from './useReleases';
 
 export function DownloadWrapper(props) {
-  const [download, setDownload] = useState<Download[]>([]);
+  const [download, setDownload] = useState<Download | null>();
   const { torrents, nzbs, nzbStatus } = useReleases();
   const { enqueueSnackbar } = useSnackbar();
 
@@ -28,6 +29,25 @@ export function DownloadWrapper(props) {
   useEffect(() => {
     getDownloads();
   }, [getDownloads]);
+
+  useSubscription(
+    'seer.downloads',
+    useCallback(
+      data => {
+        if (download == null) {
+          return;
+        }
+
+        if (download.id == data.id) {
+          download.status = data.download.status;
+          download.thash = data.download.thash;
+          download.url = data.download.url;
+          download.releaseId = data.download.releaseId;
+        }
+      },
+      [download],
+    ),
+  );
 
   return <DownloadBanner download={download} torrents={torrents} nzbs={nzbs} nzbStatus={nzbStatus} />;
 }
