12843d7c1515dcb5251778fa5475a44bfd523908
Shawn Catanzarite <me@shawncatz.com>
2023-10-06 23:24:18 -0700

    tweak size so that long titles don't break table

--- src/components/Releases/ReleasesList.tsx
+++ src/components/Releases/ReleasesList.tsx
@@ -57,7 +57,7 @@ export function ReleasesList({ data, actions }) {
                 <td>
                   <Stack spacing={1} direction="row">
                     <Link to={row.id} title={row.raw}>
-                      <div style={{ overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '575px' }}>
+                      <div style={{ overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '525px' }}>
                         <Typography variant="subtitle1" noWrap>
                           {row.display}
                         </Typography>
