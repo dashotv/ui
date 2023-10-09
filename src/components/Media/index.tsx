8e539118427cee4973b7fb2b051e76d8860889be
Shawn Catanzarite <me@shawncatz.com>
2023-10-08 21:20:23 -0700

    add links to medium banners

--- src/components/Media/index.tsx
+++ src/components/Media/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 
 import Grid from '@mui/material/Grid';
 
@@ -6,11 +7,20 @@ import MediumSmall from 'components/MediumSmall';
 import MediumBanner from 'components/MediumSmall/MediumBanner';
 
 export default function Media(props) {
+  const path = (type, id) => {
+    switch (type) {
+      case 'Episode':
+        return `/series/${id}`;
+      default:
+        return `/${type}/${id}`;
+    }
+  };
   return (
     <>
       {props.data.map(
         ({
           id,
+          type,
           series_id,
           title,
           display,
@@ -24,22 +34,24 @@ export default function Media(props) {
           broken,
         }) => (
           <Grid item key={id} md={4} xs={12}>
-            <MediumBanner
-              type={props.type}
-              id={series_id || id}
-              background={cover}
-              title={title}
-              display={display}
-              release_date={release_date}
-              active={active}
-              description={description}
-              unwatched={unwatched}
-              completed={completed}
-              cover={cover}
-              favorite={favorite}
-              broken={broken}
-              // change={change?}
-            />
+            <Link to={path(type, id)}>
+              <MediumBanner
+                type={props.type}
+                id={series_id || id}
+                background={cover}
+                title={title}
+                display={display}
+                release_date={release_date}
+                active={active}
+                description={description}
+                unwatched={unwatched}
+                completed={completed}
+                cover={cover}
+                favorite={favorite}
+                broken={broken}
+                // change={change?}
+              />
+            </Link>
           </Grid>
         ),
       )}
