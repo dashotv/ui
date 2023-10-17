7669c2da3c6daab052ed904927d67e8c7054169a
Shawn Catanzarite <me@shawncatz.com>
2023-10-16 19:50:59 -0700

    simpler notifications

--- src/components/NavBar/Notice.tsx
+++ src/components/NavBar/Notice.tsx
@@ -1,14 +1,14 @@
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
 import Typography from '@mui/material/Typography';
 
 export function Notice(props) {
   return (
-    <div className="notice">
-      <div className="title">
-        <Typography variant="overline">{props.data.class}</Typography>
+    <div>
+      <div>
+        <b>{props.data.class}</b>
       </div>
-      <div className="message">
-        <Typography variant="body1">{props.data.message}</Typography>
-      </div>
+      <div>{props.data.message}</div>
     </div>
   );
 }
