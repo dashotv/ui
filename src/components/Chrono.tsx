c023cbc20410a9e715cf38861f02a25d484fdb6f
Shawn Catanzarite <me@shawncatz.com>
2023-10-23 21:33:51 -0700

    fix chrono again, take utc out of the mix

--- src/components/Chrono.tsx
+++ src/components/Chrono.tsx
@@ -14,23 +14,24 @@ interface DateProps {
   children: string;
 }
 export default function Chrono({ fromNow, format = 'YYYY-MM-DD', special, children }: DateProps) {
-  const raw = dayjs(children).utc();
+  const raw = dayjs(children);
 
   const process = () => {
     if (special !== undefined || special) {
-      const diff = raw.diff(dayjs().utc().startOf('day'), 'days', true);
-      // console.log('raw: ', raw, 'diff: ', diff);
+      const today = dayjs().startOf('day');
+      const diff = raw.diff(today, 'days', true);
+      // console.log('today: ', today.toString(), 'raw: ', raw.toString(), 'diff: ', diff);
       if (diff > 7 || diff < -7) {
         return raw.format(format);
       }
-      if (diff == 1) {
-        return 'Tomorrow';
+      if (diff > -1 && diff < 0) {
+        return 'Yesterday';
       }
-      if (diff == 0) {
+      if (diff < 1) {
         return 'Today';
       }
-      if (diff == -1) {
-        return 'Yesterday';
+      if (diff < 2) {
+        return 'Tomorrow';
       }
       if (diff > -7 && diff < 0) {
         return 'last ' + raw.weekday(diff).format('dddd');
