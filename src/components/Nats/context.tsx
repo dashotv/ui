e553348e76ef3f930d9ecdaab6f8633bcf4fd966
Shawn Catanzarite <me@shawncatz.com>
2023-10-25 00:11:47 -0700

    try to get nats setting working in prod after vite upgrade

--- src/components/Nats/context.tsx
+++ src/components/Nats/context.tsx
@@ -1,7 +1,7 @@
 import { JSONCodec, connect } from 'nats.ws';
 import { ReactNode, createContext } from 'react';
 
-const url = import.meta.env.NODE_ENV !== 'production' ? 'ws://localhost:9222/' : 'wss://www.dasho.tv:9222/';
+const url = import.meta.env.PROD ? 'wss://www.dasho.tv:9222/' : 'ws://localhost:9222/';
 const ws = connect({ servers: url });
 const jc = JSONCodec();
 
