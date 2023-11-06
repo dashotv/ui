a499cbf9b767ca9a867e817e9130a28f0666529b
Shawn Catanzarite <me@shawncatz.com>
2023-11-06 14:14:35 -0800

    logs view

--- src/components/Logs/LogsList.tsx
+++ src/components/Logs/LogsList.tsx
@@ -1,3 +1,5 @@
+import { Typography } from '@mui/material';
+
 import Chrono from 'components/Chrono';
 import { Log } from 'types/log';
 
@@ -22,15 +24,41 @@ export function LogsList({ logs }: { logs: Log[] }) {
 }
 
 export function LogRow({ id, message, facility, level, created_at }: Log) {
+  const name = (facility: string) => {
+    const a = facility.split('::');
+    return a[a.length - 1];
+  };
+  const color = (level: string) => {
+    switch (level) {
+      case 'debug':
+        return 'text-gray-500';
+      case 'info':
+        return 'info.main';
+      case 'warn':
+        return 'warning.main';
+      case 'error':
+        return 'error.main';
+      default:
+        return 'inherit';
+    }
+  };
   return (
     <>
       <tr key={id}>
-        <td>
+        <td title={created_at}>
           <Chrono fromNow>{created_at}</Chrono>
         </td>
-        <td>{facility}</td>
-        <td>{level}</td>
-        <td>{message}</td>
+        <td title={facility}>
+          <Typography variant="button" color="secondary">
+            {name(facility)}
+          </Typography>
+        </td>
+        <td title={level}>
+          <Typography variant="button" color={color(level)}>
+            {level}
+          </Typography>
+        </td>
+        <td className="truncate">{message}</td>
       </tr>
     </>
   );
