62fa908a7a7b60f7776ed5f5554858ae1bfcde5f
Shawn Catanzarite <me@shawncatz.com>
2023-11-12 20:46:28 -0800

    lint

--- src/components/Downloads/DownloadList.tsx
+++ src/components/Downloads/DownloadList.tsx
@@ -5,14 +5,15 @@ import Grid from '@mui/material/Grid';
 
 import { DownloadBanner } from 'components/Banner';
 import { useReleases } from 'hooks/useReleases';
+import { Download } from 'types/download';
 
-export function DownloadList(props) {
+export function DownloadList({ downloads }: { downloads: Download[] }) {
   const { torrents, nzbs, nzbStatus } = useReleases();
 
   return (
     <>
-      {props.downloads &&
-        props.downloads.map(download => (
+      {downloads &&
+        downloads.map((download: Download) => (
           <Grid item key={download.id} md={4} xs={12}>
             <Link to={`/downloads/${download.id}`}>
               <DownloadBanner
