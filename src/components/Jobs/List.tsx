28ed1a034704cef4a6244cd922d93cefb24bc5d3
Shawn Catanzarite <me@shawncatz.com>
2023-11-24 01:43:29 -0800

    make jobs responsive

--- src/components/Jobs/List.tsx
+++ src/components/Jobs/List.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import ErrorIcon from '@mui/icons-material/Error';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
 
 import { Chrono } from 'components/Common';
 
@@ -9,29 +12,33 @@ import { Job } from './types';
 
 export function JobsList({ jobs }: { jobs: Job[] }) {
   return (
-    <>
-      <div className="releases">
-        <table className="vertical-table">
-          <thead>
-            <tr>
-              <td className="number"></td>
-              <td>Title</td>
-              <td className="actions" align="right">
-                Processed
-              </td>
-            </tr>
-          </thead>
-          <tbody>{jobs && jobs.map(job => <JobRow key={job.id} {...job} />)}</tbody>
-        </table>
-      </div>
-    </>
+    <Paper elevation={0}>
+      {jobs.map(job => (
+        <JobRow key={job.id} {...job} />
+      ))}
+    </Paper>
   );
 }
 
 export function JobRow({ id, name, processed_at, error }: Job) {
   return (
-    <>
-      <tr key={id}>
+    <Paper key={id} elevation={1} sx={{ mb: 1, p: 1 }}>
+      <Stack direction={{ xs: 'column', md: 'row' }} spacing={1} width="100%" alignItems="center">
+        {error === '' ? (
+          <CheckCircleIcon fontSize="small" color="success" />
+        ) : (
+          <ErrorIcon fontSize="small" color="error" />
+        )}
+        <Typography width="100%" color={error === '' ? 'primary' : 'error'} noWrap>
+          {name}
+        </Typography>
+        <Stack width="100%" direction="row" spacing={1} alignItems="center" justifyContent="end">
+          <Typography variant="subtitle2" color="gray" noWrap>
+            {processed_at ? <Chrono fromNow>{processed_at.toString()}</Chrono> : 'Pending'}
+          </Typography>
+        </Stack>
+      </Stack>
+      {/* <tr key={id}>
         <td title={error}>
           {error === '' ? (
             <CheckCircleIcon fontSize="small" color="success" />
@@ -43,7 +50,7 @@ export function JobRow({ id, name, processed_at, error }: Job) {
         <td className="actions" align="right">
           {processed_at ? <Chrono fromNow>{processed_at.toString()}</Chrono> : 'Pending'}
         </td>
-      </tr>
-    </>
+      </tr> */}
+    </Paper>
   );
 }
