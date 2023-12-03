90aa485bd6834063cc82543947c8921f1d87aaa3
Shawn Catanzarite <me@shawncatz.com>
2023-12-03 13:50:25 -0800

    add jobs dialog

---
+++ src/components/Jobs/Dialog.tsx
@@ -0,0 +1,124 @@
+import React from 'react';
+
+import CloseIcon from '@mui/icons-material/Close';
+import { Alert, AlertTitle, Dialog, DialogContent, DialogTitle, IconButton, Stack, Typography } from '@mui/material';
+
+import { Chrono } from 'components/Common';
+
+import { Job } from './types';
+
+const Args = ({ args }: { args: string }) => {
+  return (
+    <div className="args">
+      <pre style={{ fontSize: '12px', padding: '10px', backgroundColor: '#121212' }}>
+        {JSON.stringify(JSON.parse(args), null, 2)}
+      </pre>
+    </div>
+  );
+};
+
+const Attempts = ({ attempts }: { attempts: Job['attempts'] }) => {
+  return attempts && attempts.map((attempt, i) => <Attempt key={i} attempt={attempt} />);
+};
+
+const Attempt = ({
+  attempt: { status, started_at, duration, error, stacktrace },
+  key,
+}: {
+  key: number;
+  attempt: Job['attempts'][0];
+}) => {
+  return (
+    <div className="attempt" style={{ padding: '8px' }}>
+      <Stack key={key} direction="row" spacing={1} alignItems="center" justifyContent="start" width="100%">
+        <Typography variant="subtitle1" color={status === 'failed' ? 'error' : 'action'}>
+          {status}
+        </Typography>
+        <Stack width="100%" direction="row" spacing={1} alignItems="center" justifyContent="end">
+          <Typography variant="caption" color="action">
+            {duration ? `${duration.toFixed(1)}s` : ''}
+          </Typography>
+          <Typography minWidth="100px" variant="subtitle2" color="gray" noWrap>
+            {started_at ? <Chrono fromNow>{started_at.toString()}</Chrono> : 'Pending'}
+          </Typography>
+        </Stack>
+      </Stack>
+      <Stacktrace {...{ error, stacktrace }} />
+    </div>
+  );
+};
+
+const Stacktrace = ({ error, stacktrace }: { error: string; stacktrace: string[] }) => {
+  if (!error) return null;
+  return (
+    <Alert severity="error">
+      <AlertTitle>{error}</AlertTitle>
+      <div role="alert">
+        <Stack direction="column">{stacktrace && stacktrace.map((trace, i) => <Trace key={i} trace={trace} />)}</Stack>
+      </div>
+    </Alert>
+  );
+};
+const Trace = ({ trace }: { trace: Job['attempts'][0]['stacktrace'][0] }) => {
+  return (
+    <Typography variant="subtitle2" color="action">
+      {trace}
+    </Typography>
+  );
+};
+
+export const JobsDialog = ({ close, job: { id, kind, status, args, attempts } }: { close: () => void; job: Job }) => {
+  const [open, setOpen] = React.useState(true);
+  const handleClose = () => {
+    setOpen(false);
+    close();
+  };
+  return (
+    <Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth>
+      <DialogTitle>
+        <Stack direction="row" spacing={3} alignItems="center" justifyContent="start" width="100%">
+          <Typography variant="h6" color="primary">
+            {kind}
+          </Typography>
+        </Stack>
+        <IconButton
+          aria-label="close"
+          onClick={handleClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: theme => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      </DialogTitle>
+      <DialogContent>
+        <Stack width="100%" direction={{ xs: 'column', md: 'row' }} spacing={1}>
+          <Typography minWidth="125px" variant="subtitle1" color="textSecondary">
+            Status
+          </Typography>
+          <Typography variant="subtitle1" color={status === 'failed' ? 'error' : 'success'}>
+            {status}
+          </Typography>
+        </Stack>
+        <Stack width="100%" direction={{ xs: 'column', md: 'row' }} spacing={1}>
+          <Typography minWidth="125px" variant="subtitle1" color="textSecondary">
+            ID
+          </Typography>
+          <Typography variant="button">{id}</Typography>
+        </Stack>
+
+        <Typography variant="subtitle1" color="textSecondary">
+          Args
+        </Typography>
+        <Args {...{ args }} />
+        <Typography variant="subtitle1" color="textSecondary">
+          Attempts
+        </Typography>
+        <Attempts {...{ attempts }} />
+      </DialogContent>
+    </Dialog>
+  );
+};
--- src/components/Jobs/List.tsx
+++ src/components/Jobs/List.tsx
@@ -3,36 +3,48 @@ import React from 'react';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import ErrorIcon from '@mui/icons-material/Error';
 import PendingIcon from '@mui/icons-material/Pending';
+import { Link } from '@mui/material';
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 
 import { Chrono } from 'components/Common';
 
+import { JobsDialog } from './Dialog';
 import { Job } from './types';
 
 export function JobsList({ jobs }: { jobs: Job[] }) {
+  const [selected, setSelected] = React.useState<Job | null>(null);
+  const open = (job: Job) => {
+    setSelected(job);
+  };
+  const close = () => {
+    setSelected(null);
+  };
   return (
     <Paper elevation={0}>
       {jobs.map(job => (
-        <JobRow key={job.id} {...job} />
+        <Link key={job.id} href="#" onClick={() => open(job)}>
+          <JobRow {...job} />
+        </Link>
       ))}
+      {selected && <JobsDialog job={selected} close={close} />}
     </Paper>
   );
 }
+const Icon = ({ status }: { status: string }) => {
+  switch (status) {
+    case 'failed':
+      return <ErrorIcon fontSize="small" color="error" />;
+    case 'finished':
+      return <CheckCircleIcon fontSize="small" color="success" />;
+    default:
+      return <PendingIcon fontSize="small" color="disabled" />;
+  }
+};
 
 export function JobRow({ id, kind, status, attempts }: Job) {
   const { started_at, duration, error } = (attempts && attempts.length > 0 && attempts[attempts.length - 1]) || {};
-  const Icon = ({ status }: { status: string }) => {
-    switch (status) {
-      case 'failed':
-        return <ErrorIcon fontSize="small" color="error" />;
-      case 'finished':
-        return <CheckCircleIcon fontSize="small" color="success" />;
-      default:
-        return <PendingIcon fontSize="small" color="disabled" />;
-    }
-  };
   return (
     <Paper key={id} elevation={1} sx={{ mb: 1, p: 1 }}>
       <Stack direction={{ xs: 'column', md: 'row' }} spacing={1} width="100%" alignItems="center">
