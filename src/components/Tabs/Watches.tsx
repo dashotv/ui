cb441e0b348b5f0f121c497d4a2407e8fa111819
Shawn Catanzarite <me@shawncatz.com>
2024-01-08 16:42:20 -0800

    watches tab shouldn't be all watches

--- src/components/Tabs/Watches.tsx
+++ src/components/Tabs/Watches.tsx
@@ -4,7 +4,7 @@ import { LoadingIndicator, WrapErrorBoundary } from 'components/Common';
 import { WatchesList, useWatchesQuery } from 'components/Watches';
 
 export function Watches({ medium_id }: { medium_id: string }) {
-  const { isFetching, data } = useWatchesQuery(medium_id);
+  const { isFetching, data } = useWatchesQuery(1, medium_id);
   return (
     <WrapErrorBoundary>
       {isFetching && <LoadingIndicator />}
--- src/components/Watches/query.ts
+++ src/components/Watches/query.ts
@@ -4,15 +4,15 @@ import { useQuery } from '@tanstack/react-query';
 
 import { Watch } from './types';
 
-export const getWatches = async page => {
-  const response = await tower.get(`/watches/?page=${page}`);
+export const getWatches = async (page: number, medium_id?: string) => {
+  const response = await tower.get(`/watches/?page=${page}&medium_id=${medium_id ? medium_id : ''}`);
   return response.data as Watch[];
 };
 
-export const useWatchesQuery = page =>
+export const useWatchesQuery = (page: number, medium_id?: string) =>
   useQuery({
-    queryKey: ['watches', page],
-    queryFn: () => getWatches(page),
+    queryKey: ['watches', page, medium_id],
+    queryFn: () => getWatches(page, medium_id),
     placeholderData: previousData => previousData,
     retry: false,
   });
