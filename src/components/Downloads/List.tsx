bfd84d1fa8948792a885b770d5977a9515b153d1
Shawn Catanzarite <me@shawncatz.com>
2024-01-27 21:06:39 -0800

    fix errors when new downloads are added

--- src/components/Downloads/List.tsx
+++ src/components/Downloads/List.tsx
@@ -11,40 +11,39 @@ export function DownloadList({ downloads }: { downloads: DownloadType[] }) {
   const { get } = useDownloading();
   return (
     <>
-      {downloads?.map(
-        ({ id, status, multi, medium: { title, display: subtitle, cover, background } }: DownloadType) => {
-          const {
-            progress,
-            eta,
-            queue,
-            files: { completed: files, selected: total },
-            torrent_state: torrentState,
-          } = get(id);
-          return (
-            <Grid item key={id} md={4} xs={12}>
-              <Link to={`/downloads/${id}`}>
-                <DownloadBanner
-                  {...{
-                    id,
-                    title,
-                    subtitle,
-                    cover,
-                    background,
-                    status,
-                    torrentState,
-                    progress,
-                    eta,
-                    queue,
-                    multi,
-                    files,
-                    total,
-                  }}
-                />
-              </Link>
-            </Grid>
-          );
-        },
-      )}
+      {downloads?.map(({ id, status, multi, medium }: DownloadType) => {
+        const {
+          progress,
+          eta,
+          queue,
+          files: { completed: files, selected: total },
+          torrent_state: torrentState,
+        } = get(id);
+        const { title, display: subtitle, cover, background } = medium || {};
+        return (
+          <Grid item key={id} md={4} xs={12}>
+            <Link to={`/downloads/${id}`}>
+              <DownloadBanner
+                {...{
+                  id,
+                  title,
+                  subtitle,
+                  cover,
+                  background,
+                  status,
+                  torrentState,
+                  progress,
+                  eta,
+                  queue,
+                  multi,
+                  files,
+                  total,
+                }}
+              />
+            </Link>
+          </Grid>
+        );
+      })}
     </>
   );
 }
