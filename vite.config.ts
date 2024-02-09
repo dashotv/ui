5ea1635c56955c6603071864087c8b21f2684c9d
Shawn Catanzarite <me@shawncatz.com>
2024-02-09 13:50:59 -0800

    don't commit plex token to git oops

--- vite.config.ts
+++ vite.config.ts
@@ -1,78 +1,81 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  // depending on your application, base can also be "/"
-  base: '/',
-  plugins: [react(), nodePolyfills(), viteTsconfigPaths()],
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [esbuildCommonjs(['react-moment'])],
-    },
-  },
-  //   test: {
-  //     globals: true,
-  //     environment: 'jsdom',
-  //     setupFiles: './src/setupTests.ts',
-  //     css: true,
-  //     reporters: ['verbose'],
-  //     coverage: {
-  //       reporter: ['text', 'json', 'html'],
-  //       include: ['src/**/*'],
-  //       exclude: [],
-  //     },
-  //   },
-  server: {
-    // this ensures that the browser opens upon server start
-    open: true,
-    // this sets a default port to 3000
-    port: 3000,
-    proxy: {
-      '/thumbs': {
-        target: 'http://10.0.4.61:32400',
-        changeOrigin: true,
-        secure: false,
-        ws: false,
-        rewrite: path => path.replace(/^\/thumbs\/(.*)/, '/$1?X-Plex-Token=***REMOVED***'),
-      },
-      '/api/tower': {
-        target: 'http://localhost:9000',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-        rewrite: path => path.replace(/^\/api\/tower/, ''),
-      },
-      '/api/flame': {
-        target: 'http://localhost:9001',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-        rewrite: path => path.replace(/^\/api\/flame/, ''),
-      },
-      '/api/runic': {
-        target: 'http://localhost:9002',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-        rewrite: path => path.replace(/^\/api\/runic/, ''),
-      },
-      '/api/scry': {
-        target: 'http://localhost:10080',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-        rewrite: path => path.replace(/^\/api\/scry/, ''),
-      },
-      '/media-images': {
-        target: 'http://seer.dasho.net/',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
+    // depending on your application, base can also be "/"
+    base: '/',
+    plugins: [react(), nodePolyfills(), viteTsconfigPaths()],
+    optimizeDeps: {
+      esbuildOptions: {
+        plugins: [esbuildCommonjs(['react-moment'])],
       },
     },
-  },
-});
+    //   test: {
+    //     globals: true,
+    //     environment: 'jsdom',
+    //     setupFiles: './src/setupTests.ts',
+    //     css: true,
+    //     reporters: ['verbose'],
+    //     coverage: {
+    //       reporter: ['text', 'json', 'html'],
+    //       include: ['src/**/*'],
+    //       exclude: [],
+    //     },
+    //   },
+    server: {
+      // this ensures that the browser opens upon server start
+      open: true,
+      // this sets a default port to 3000
+      port: 3000,
+      proxy: {
+        '/thumbs': {
+          target: process.env.VITE_APP_PLEX_URL,
+          changeOrigin: true,
+          secure: false,
+          ws: false,
+          rewrite: path => path.replace(/^\/thumbs\/(.*)/, '/$1?' + process.env.VITE_APP_PLEX_QUERY),
+        },
+        '/api/tower': {
+          target: 'http://localhost:9000',
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+          rewrite: path => path.replace(/^\/api\/tower/, ''),
+        },
+        '/api/flame': {
+          target: 'http://localhost:9001',
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+          rewrite: path => path.replace(/^\/api\/flame/, ''),
+        },
+        '/api/runic': {
+          target: 'http://localhost:9002',
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+          rewrite: path => path.replace(/^\/api\/runic/, ''),
+        },
+        '/api/scry': {
+          target: 'http://localhost:10080',
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+          rewrite: path => path.replace(/^\/api\/scry/, ''),
+        },
+        '/media-images': {
+          target: 'http://seer.dasho.net/',
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+        },
+      },
+    },
+  });
+};
