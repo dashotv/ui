ee0d0fce5a0c1b6e2aa8d0f334d3362d6d24b070
Shawn Catanzarite <me@shawncatz.com>
2024-06-09 23:47:54 +0000

    pagination on requests

--- src/pages/AdminPage/RequestsPage.tsx
+++ src/pages/AdminPage/RequestsPage.tsx
@@ -1,17 +1,27 @@
 import React, { useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 
-import { LoadingIndicator } from '@dashotv/components';
+import { Grid } from '@mui/material';
+
+import { LoadingIndicator, Pagination } from '@dashotv/components';
 import { Container } from '@dashotv/components';
 import { useQueryClient } from '@tanstack/react-query';
 
 import { RequestsList, useRequestsQuery } from 'components/Requests';
 import { useSub } from 'hooks/sub';
 
+const pagesize = 25;
 export default function RequestsPage() {
-  const [page] = useState(1);
-  const requests = useRequestsQuery(page);
+  const [page, setPage] = useState(1);
   const queryClient = useQueryClient();
+
+  const { isFetching, data: requests } = useRequestsQuery(page);
+  const total = requests?.total || 0;
+  const pages = Math.ceil(total / pagesize);
+  const onChange = (e: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
+
   useSub('tower.requests', () => {
     console.log('tower.requests');
     queryClient.invalidateQueries({ queryKey: ['requests', page] });
@@ -24,8 +34,14 @@ export default function RequestsPage() {
         <meta name="description" content="dashotv" />
       </Helmet>
       <Container>
-        {requests.isFetching && <LoadingIndicator />}
-        {requests.data && <RequestsList requests={requests.data.result} />}
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6}></Grid>
+          <Grid item xs={12} md={6} justifyContent="end">
+            <Pagination page={page} total={total} count={pages} onChange={onChange} />
+          </Grid>
+        </Grid>
+        {isFetching && <LoadingIndicator />}
+        {requests ? <RequestsList requests={requests.result} /> : null}
       </Container>
     </>
   );
